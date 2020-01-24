f4197302b9c6a9d8c7ce987e8d4b80ba9cf32b31
pdtunes <55880640+pdtunes@users.noreply.github.com>
2020-01-24 13:33:42 +0100

    Update peder-lundberg-js1-ma1.js

--- peder-lundberg-js1-ma1.js
+++ peder-lundberg-js1-ma1.js
@@ -6,20 +6,20 @@ Create an object called cat.
 Give the object one property called complain. 
 complain's value should be a method (a function) which logs the string "Meow!". */
 
-var cat = {
-   function complain("meow!"){
-       console.log("meow!");
-   }
-}
+let cat = {
+  complain: function() {
+    console.log("Meow!");
+  }
+};
 
-complain("meow!)
-
-  
+cat.complain()
 
 
 /* Question 2
 Select the h3 from the HTML below using the querySelector method and assign it to a variable called heading.
 <h3>Subheading</h3> */
+
+let heading 
 
 /* Question 3
 Use the style property on the heading variable from the question above to change its font size to "2em". */
@@ -53,7 +53,6 @@ const cats = [
     }
 ]; */
 
-
 /* Question 8
 Using the function and cats variable from the above question, instead of logging the name property, wrap each name property in an h5 tag, add it to a variable you declare before the loop and return the variable from the function after the loop.
 The function should return the following:
@@ -64,7 +63,6 @@ The function should return the following:
 /* Question 9
 Call the function from question 8, pass it the cats variable from question 7 and set the inner HTML of the resultsContainer variable from question 6 to the return value of the function.
  */
-
 
 /* Question 10
 Using the function from question 8, add a p element containing the age property from each object. If the age property is missing, it should display Age unknown instead.
